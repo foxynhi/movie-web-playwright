16494a335f9d44efb98ec659daccb9f72f476926
foxynhi <trinitykn@protonmail.com>
2025-10-10 10:05:20 +0700

    test: add coverage for different browsers

---
+++ fixtures/interfaces.ts
@@ -0,0 +1,37 @@
+import type { ReportGenerator } from "../utils/reportGenerator";
+
+export interface StepTime {
+  step: string;
+  duration: number;
+}
+export type StepTimes = StepTime[];
+
+export type TestStatus =
+  | "passed"
+  | "failed"
+  | "skipped"
+  | "timedOut"
+  | "interrupted"
+  | undefined;
+
+export interface TestResult {
+  testName: string;
+  status: TestStatus;
+  duration: number;
+  startTime: number;
+  steps: StepTimes;
+  browser?: string;
+  testFile: string;
+  error?: string | null;
+}
+
+export interface TestCredentials {
+  email: string;
+  password: string;
+  baseUrl: string;
+}
+
+export interface ReportGeneratorFixture {
+  reportGenerator: ReportGenerator;
+  testResult: TestResult;
+}
